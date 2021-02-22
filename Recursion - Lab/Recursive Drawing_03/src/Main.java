f187c0163c71b4201cd81817ea3dc1077dd9e269
Delyan Milchev <d_milchev99@abv.bg>
2021-02-22 16:52:56 +0200

    Add task Recursive Drawing

---
+++ Recursion - Lab/Recursive Drawing_03/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        drawFigure(number);
+    }
+
+    private static void drawFigure(int number) {
+        if(number == 0) {
+            return;
+        }
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        drawFigure(number - 1);
+
+        for (int i = 0; i < number; i++) {
+            System.out.print("#");
+        }
+        System.out.println();
+    }
+}
